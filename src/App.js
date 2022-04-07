edeea785fcd058d19f8d3c3bc6973b05b5a0563a
Left-Behind-Sic <95462486+Left-Behind-Sic@users.noreply.github.com>
2022-04-07 14:16:10 +0500

    react ulbitv

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from "react";
 import './App.css';
 import {useState} from "react";
 
 function App() {
     const [count, setCount] = useState(0);
 
+    function increment() {
+        setCount(count+1)
+    }
+
+    function decrement() {
+        setCount(count-1)
+    }
 
     return (
         <div className="App">
             <h1>{count}</h1>
-            <button onClick={() => setCount(count+1)}>push</button>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
         </div>
     );
 }
