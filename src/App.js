312cdc94feedad164b9a4e559cdb0c3add7311f3
Left-Behind-Sic <95462486+Left-Behind-Sic@users.noreply.github.com>
2022-04-07 16:34:06 +0500

    react 07.04

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from "react";
-import './App.css';
+import './styles/App.css';
 import {useState} from "react";
+import PostList from "./Components/PostList";
+import MyButton from "./Components/UI/button/MyButton";
+import MyInput from "./Components/UI/input/MyInput";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Javascript", body: "Javascript - язык программирования"},
+        {id: 2, title: "Javascript 2", body: "Javascript - язык программирования"},
+        {id: 3, title: "Javascript 3", body: "Javascript - язык программирования"},
+    ])
 
-    function increment() {
-        setCount(count+1)
-    }
-
-    function decrement() {
-        setCount(count-1)
+    const [title, setTitle] =useState('')
+    function addNewPost(e){
+        e.preventDefault();
+        console.log(title)
     }
 
     return (
         <div className="App">
-            <h1>{count}</h1>
-            <button onClick={increment}>increment</button>
-            <button onClick={decrement}>decrement</button>
+
+            <form >
+                <MyInput
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    type="text"
+                    placeholder="Название"
+                />
+                <MyInput type="text" placeholder="Описание"/>
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title="JS"/>
         </div>
     );
 }
